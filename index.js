a5bc7e68070ed47097feb2a9fe9a0448b74bc413
yaoshicheng <929239151@qq.com>
2019-05-16 12:56:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-var stackChart ;
-
 function getTimingData(){
 	var timing = window.performance.timing;
 	var dns = timing.domainLookupEnd  - timing.domainLookupStart;
@@ -14,47 +12,7 @@ function getTimingData(){
 	})
 	return [dns,tcp,request,domresolve,whiteScreen,domready,onload]
 }
-var data = getTimingData();
-option = {
-    tooltip : {
-        trigger: 'axis',
-        axisPointer : {            // 坐标轴指示器，坐标轴触发有效
-            type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
-        }
-    },
-    legend: {
-        data: ['DNS查询耗时 ','TCP链接耗时','request请求耗时','解析dom树耗时','白屏时间','domready时间','onload时间']
-    },
-    grid: {
-        left: '3%',
-        right: '4%',
-        bottom: '3%',
-        containLabel: true
-    },
-    xAxis:  {
-        type: 'value'
-    },
-    yAxis: {
-        type: 'category',
-        data: ['DNS查询耗时 ','TCP链接耗时','request请求耗时','解析dom树耗时','白屏时间','domready时间','onload时间']
-    },
-    series: [
-        {
-            type: 'bar',
-            label: {
-                normal: {
-                    show: true,
-                    position: 'insideRight'
-                }
-            },
-            data: data
-        },
-    ]
-};
-let container = document.getElementById('stackContainer');
 
-if(stackChart){
-	stackChart.dispose();
-}
-stackChart = echarts.init(container);
-stackChart.setOption(option);
+var legend= {
+        data: ['DNS查询耗时 ','TCP链接耗时','request请求耗时','解析dom树耗时','白屏时间','domready时间','onload时间']
+    };
